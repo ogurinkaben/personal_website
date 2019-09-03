18cf83c197934192c32c1f0c01933ecb48e370f6
simplytammy <ogurinkabenjamin@gmail.com>
2019-09-03 10:17:39 +0100

    sleek update

---
+++ app.js
@@ -0,0 +1,26 @@
+function toggleTheme( ) {
+	var element = document.getElementById( "body" );
+	var toggleIcon = document.getElementById( "toggle" );
+	element.classList.toggle( "theme" );
+	if ( toggleIcon.classList.contains( 'fa-sun' ) ) {
+		toggleIcon.classList.remove( 'fa-sun' );
+		toggleIcon.classList.add( 'fa-moon' );
+	}
+	else {
+		toggleIcon.classList.add( 'fa-sun' );
+		toggleIcon.classList.remove( 'fa-moon' );
+	}
+}
+
+function openNav( ) {
+	document.getElementById( "myNav" ).style.width = "100%";
+}
+
+function closeNav( ) {
+	document.getElementById( "myNav" ).style.width = "0%";
+}
+
+function viewMore( ) {
+	var more = document.getElementById( "moreContent" );
+	more.classList.toggle( "active" );
+}
