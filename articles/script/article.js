6d3bd64c6118f247d31c703e189750ef83b301bb
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2020-04-22 23:49:24 +0100

    Back to top

--- articles/script/article.js
+++ articles/script/article.js
@@ -40,7 +40,31 @@ const setUserLink = (id, hasValue, url) => {
 }
 getArticle();
 const speedup = () => {
-    let speed = getComputedStyle(document.body).getPropertyValue('--user-img-speed');
-    speed = parseFloat(speed.replace('s', '')) / 3 + 's'
-    document.documentElement.style.setProperty('--user-img-speed', speed);
-}
+        let speed = getComputedStyle(document.body).getPropertyValue('--user-img-speed');
+        speed = parseFloat(speed.replace('s', '')) / 3 + 's'
+        document.documentElement.style.setProperty('--user-img-speed', speed);
+    }
+    (function() {
+        'use strict';
+
+        function trackScroll() {
+            var scrolled = window.pageYOffset;
+            var coords = document.documentElement.clientHeight;
+            if (scrolled > coords) {
+                goTopBtn.classList.add('back_to_top-show');
+            }
+            if (scrolled < coords) {
+                goTopBtn.classList.remove('back_to_top-show');
+            }
+        }
+
+        function backToTop() {
+            if (window.pageYOffset > 0) {
+                window.scrollBy(0, -80);
+                setTimeout(backToTop, 0);
+            }
+        }
+        var goTopBtn = document.querySelector('.back_to_top');
+        window.addEventListener('scroll', trackScroll);
+        goTopBtn.addEventListener('click', backToTop);
+    })();
