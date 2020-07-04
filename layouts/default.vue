0104d865433642bce8868c9b135d520963e5c1dd
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2020-07-04 03:52:37 +0100

    Update page transitions

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,11 @@
 <template>
-  <div>
-    <vue-page-transition name="fade-in-up">
-      <Nuxt />
-    </vue-page-transition>
-  </div>
+	<div>
+		<Nuxt />
+	</div>
 </template>
 <script>
-import Vue from 'vue'
-import VuePageTransition from 'vue-page-transition'
-Vue.use(VuePageTransition)
 export default {
-  name: 'App',
-  transition: 'fade-in-up',
-  scrollToTop: true,
+	name: 'App',
 }
 
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,42 +1,46 @@
 export default {
-  loading: {
-    color: 'rgb(23, 191, 99)',
-    height: '4px'
-  },
-  generate: {
-    fallback: true
-  },
-  mode: 'universal',
-  target: 'static',
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  css: [
-    '~/assets/main.css'
-  ],
-  plugins: [
-    { src: '~/plugins/sweetAlert.js', mode: 'client' },
-  ],
-  components: true,
-  buildModules: [
-    '@nuxtjs/eslint-module'
-  ],
-  modules: [
-    '@nuxtjs/axios',
-    'vue-scrollto/nuxt',
-    ['vue-scrollto/nuxt', { duration: 1000 }],
-    ['nuxt-lazy-load', {
-      defaultImage: '~/assets/img/page-images/placeholder-image.png',
-    }]
-  ],
-  axios: {},
-  build: {}
+	transition: {
+		name: 'zoom',
+		mode: 'out-in'
+	},
+	loading: {
+		color: 'rgb(23, 191, 99)',
+		height: '4px'
+	},
+	generate: {
+		fallback: true
+	},
+	mode: 'universal',
+	target: 'static',
+	head: {
+		title: process.env.npm_package_name || '',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+		]
+	},
+	css: [
+		'~/assets/main.css'
+	],
+	plugins: [
+		{ src: '~/plugins/sweetAlert.js', mode: 'client' },
+	],
+	components: true,
+	buildModules: [
+		'@nuxtjs/eslint-module'
+	],
+	modules: [
+		'@nuxtjs/axios',
+		'vue-scrollto/nuxt',
+		['vue-scrollto/nuxt', { duration: 1000 }],
+		['nuxt-lazy-load', {
+			defaultImage: '~/assets/img/page-images/placeholder-image.png',
+		}]
+	],
+	axios: {},
+	build: {}
 }
