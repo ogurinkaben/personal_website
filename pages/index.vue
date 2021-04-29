d98fce8e6a68b8cde419da1a47291a929fe93243
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2021-04-29 14:08:47 +0100

    Update intro text

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <div class="og-hero--content home og-container">
         <div class="animate__animated animate__fadeInUp">
           <h1 class="title  animate__animated animate__bounce uu">
-            I Design Interactive Applications that define experiences, products, and brands<span class="period"></span>
+            I Design & Develop Interactive Applications that define Experiences, Products, and Brands<span class="period"></span>
           </h1>
           <div>
             <nuxt-link class="og-link--button" to="/contact">
