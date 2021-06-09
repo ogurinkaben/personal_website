61eb818ab761ea68c483dd881487b77d2bea9a16
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2021-06-09 14:35:49 +0100

    Product detail

--- pages/products/a2z.vue
+++ pages/products/a2z.vue
@@ -7,13 +7,62 @@
           <h1 class="title">Minimal and Powerful link tracking and analytics for you</h1>
           <p class="info">a2z (formerly everylink) is a super simple URL shortener/tracker made with ❤️ and specially designed and developed for content creators to give them a fast and simple way to easily share clean trackable content URLs</p>
           <div class="pt">
-            <nuxt-link class="og-button bold" to="/contact">Request Access</nuxt-link>
+            <nuxt-link class="btn-grad" to="/contact">Request Access</nuxt-link>
           </div>
         </div>
-          <span class="scroll"></span>
+        <span class="scroll"></span>
       </div>
 
     </section>
+    <div class="">
+      <section class="og-section is--flex">
+        <div class="og-container">
+          <div class="row start" data-aos="slide-up" data-aos-easing="ease">
+            <div class="column">
+              <div class="product-info">
+                <h3>Simple</h3>
+                <p>
+                  It's simple to use and interact with. a2z was created with the user in mind, with a simple interface that allows you to concentrate on what you need to do.
+                </p>
+              </div>
+            </div>
+            <div class="column">
+              <div class="product-info">
+                <h3>Boost results</h3>
+                <p>Discover who is clicking on your links. Knowing when and on which devices people interact with your content can help you make better decisions.</p>
+              </div>
+            </div>
+            <div class="column">
+              <div class="product-info">
+                <h3>Free Forever</h3>
+                <p>
+                  a2z is and always will be free. You do not need to pay anything; simply create your links and you are ready to take advantage of a variety of awesome features.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="og-section is--flex sm">
+        <div class="og-container is-flex flex-center justify-center">
+          <img src="../../assets/img/products/a2z/link.png" class="image" alt="">
+        </div>
+      </section>
+      <div class="divider"></div>
+      <div class="bg--dark">
+
+        <div class="og-section  is-flex flex-center justify-center text-is-center ">
+          <div class="og-section--heading">
+            <h1 class="title text-is-white">Get more out of your links with powerful tools to assist you in growing and protecting your brand.</h1>
+            <div class=" text-is-center ">
+              <div class="pt">
+                <nuxt-link class="btn-grad" to="/contact">Request Access</nuxt-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <Footer />
   </div>
 </template>
@@ -74,5 +123,37 @@
 <style>
   .products {
     background: var(--primaryColor);
+  }
+
+  .products .og-hero {
+    background: linear-gradient(to bottom, rgba(0, 9, 27, 0.7), rgba(0, 9, 27, 0.7)),
+      url('../../assets/img/bg/a2z_bg-full.png') !important;
+    background-position: center center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    padding: 1rem;
+  }
+
+  @media only screen and (max-width: 960px) {
+    .products .og-hero {
+      background: linear-gradient(to bottom, rgba(0, 9, 27, 0.7), rgba(0, 9, 27, 0.7)),
+        url('../../assets/img/bg/a2z_bg-mobile.png') !important;
+    }
+  }
+
+  .product-info {
+    padding: 1rem;
+  }
+
+  .product-info p {
+    color: #808080;
+    font-size: 1rem;
+    opacity: 0.;
+  }
+
+  .product-info h3 {
+    font-size: 1.2rem;
+    color: #fff;
+    margin-bottom: 1rem;
   }
 </style>
